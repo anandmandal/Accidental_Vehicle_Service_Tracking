fbe144a5e121eb353d1391946f07e89d37a7e417
Anand-Kumar-Mandal <125601960+Anand-Kumar-Mandal@users.noreply.github.com>
2024-05-09 11:33:18 +0530

    last second change

--- backend/models/fileModel.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
-from pymongo import MongoClient
-
-class FileModel(BaseModel):
-    files: UploadFile
-    fileName: str
-    fileDescription: str
-
